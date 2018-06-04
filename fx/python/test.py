f6742dddd5cfe9e0e8be4f4f934e53b61b9ae554
Feng J. Lang <roulang_2008@sina.com>
2018-06-05 00:42:02 +0800

    add test.py

---
+++ fx/python/test.py
@@ -0,0 +1,37 @@
+#|----------------------------------------------
+#|this program is test for python
+#|----------------------------------------------
+
+import random,sys,os,math;
+
+a='abc';
+print(a);
+if (a=='ab'):
+    print(a);
+elif (a=='abc'):
+    print('ab');
+    print('abc');
+    print('aa');
+else :
+    print('N/A');
+"""
+for i in range(5):
+    print(random.randint(1,10));
+sys.exit();
+def hello(name):
+    print('hello '+name);
+hello('hey');
+hello('me');
+"""
+"""
+def spam(divideBy):
+    try:
+        return 42/divideBy;
+    except ZeroDivisionError:
+        print('Error: Invalid argument.')
+
+print(spam(1));
+print(spam(2));
+print(spam(0));
+print(spam(7));
+"""
