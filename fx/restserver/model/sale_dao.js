44155a98ede1253ae0062638d6e190767be44ed8
Feng J. Lang <roulang_2008@sina.com>
2018-06-04 23:05:07 +0800

    add fx files

---
+++ fx/restserver/model/sale_dao.js
@@ -0,0 +1,21 @@
+﻿
+function sale_dao(){
+	
+	this.retrieve = function(id, params, callback){
+		var sales = {
+			"catg": ['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子'],
+			"data": [5, 20, 36, 10, 10, 20]
+		};
+		callback(sales);
+	};
+	
+	this.list = function(id, params, callback){
+		var sales = {
+			"catg": ['衬衫','羊毛衫','雪纺衫','裤子','高跟鞋','袜子'],
+			"data": [5, 20, 36, 10, 10, 20]
+		};
+		callback(sales);
+	};
+}
+
+exports.dao = sale_dao;
---
+++ fx/restserver/test.js
@@ -0,0 +1,9 @@
+var sqlite3 = require('sqlite3').verbose();
+var file = "D:/rou/sync/db/abc.db";
+var db = new sqlite3.Database(file);
+db.all("select key from cot", function(err, rows) {
+        rows.forEach(function (row) {
+            console.log(row.key);
+        })
+	});	
+db.close();
