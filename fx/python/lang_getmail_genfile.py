14eeec23bf18f1c717ff86f30ea907993482800e
Feng J. Lang <roulang_2008@sina.com>
2018-06-24 21:11:55 +0800

    mod python add real

--- fx/python/lang_getmail_genfile.py
+++ fx/python/lang_getmail_genfile.py
@@ -9,6 +9,7 @@ curs = ['USD', 'EUR', 'JPY', 'AUD', 'NZD', 'CAD', 'GBP', 'CHF']
 
 clt_dev = '50CA3DFB510CC5A8F28B48D1BF2A5702'
 clt_test = '2010C2441A263399B34F537D91A53AC9'
+clt_real = 'BB190E062770E27C3E79391AB0D1A117'
 
 imapserver = 'imap.163.com'
 account = 'roulang_2018@163.com'
@@ -49,6 +50,7 @@ smtpObj = SMTP_SSL(smtpserver)
 smtpObj.login(account, pwd)
 
 pt = 'C:/Users/lang/AppData/Roaming/MetaQuotes/Terminal/' + clt_test + '/MQL4/Files/'
+pt2 = 'C:/Users/lang/AppData/Roaming/MetaQuotes/Terminal/' + clt_real + '/MQL4/Files/'
 for msg in msgs:
     cur = msg['subject'].upper()
     cur1 = cur[0:3]
@@ -56,10 +58,16 @@ for msg in msgs:
     if ((cur1 not in curs) or (cur2 not in curs)):
     	continue
     com = msg['body']
+
     fn = pt + 'lang_' + cur
     f = open(fn, 'w', newline='')
     f.write(com)
     f.close()
+    fn2 = pt2 + 'lang_' + cur
+    f2 = open(fn2, 'w', newline='')
+    f2.write(com)
+    f2.close()
+
     sub = cur + ' is set to ' + com
     rep = MIMEText('')
     rep['Subject'] = Header(sub)
