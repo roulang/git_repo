c82e793c7eeb875d2b719774fe05aea8d5717a7c
Feng J. Lang <roulang_2008@sina.com>
2018-07-01 16:25:22 +0800

    mod imp html

--- fx/restserver/model/imp.js
+++ fx/restserver/model/imp.js
@@ -1,4 +1,5 @@
 ﻿var curs = {'EURUSD':1, 'USDJPY':-1, 'AUDUSD':1, 'NZDUSD':1, 'USDCAD':-1, 'GBPUSD':1, 'USDCHF':-1};
+var digs = {'EURUSD':10000, 'USDJPY':100, 'AUDUSD':10000, 'NZDUSD':10000, 'USDCAD':10000, 'GBPUSD':10000, 'USDCHF':10000};
 
 function imp(dtm, country, title, cur, ped, open_p, close_p, high_p, low_p){
 	this.dtm = dtm;
@@ -12,6 +13,8 @@ function imp(dtm, country, title, cur, ped, open_p, close_p, high_p, low_p){
 	this.low_p = low_p;
 	this.diff_oc = Math.round((close_p - open_p)/open_p*10000)*curs[cur]/100;
 	this.diff_hl = Math.round((high_p - low_p)/open_p*10000)/100;
+	this.diff_oc2 = Math.round((close_p - open_p)*digs[cur]);
+	this.diff_hl2 = Math.round((high_p - low_p)*digs[cur]);
 }
 
 exports.imp=imp;
