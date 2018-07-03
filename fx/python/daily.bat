966aa0f7f9809b569014bfb5156e28a006eff723
Feng J. Lang <roulang_2008@sina.com>
2018-07-03 22:34:40 +0800

    mod daily batch

--- fx/python/daily.bat
+++ fx/python/daily.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Python36\python.exe" "D:\rou\sync\workspace\fx\python\lang_gen_news_file.py"
 "C:\Program Files\Python36\python.exe" "D:\rou\sync\workspace\fx\python\lang_getnews2.py"
+"C:\Program Files\Python36\python.exe" "D:\rou\sync\workspace\fx\python\lang_gen_news_file.py"
