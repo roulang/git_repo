ec07c9d283bff18aca02ef9e43ea3f3c317a467f
Feng J. Lang <roulang_2008@sina.com>
2018-07-09 01:02:12 +0800

    add fund

---
+++ fx/restserver/model/fund.js
@@ -0,0 +1,12 @@
+﻿
+function fund(code, name2, type, mgr, com, st, trace){
+	this.code = code;
+	this.name2 = name2;
+	this.type = type;
+	this.mgr = mgr;
+	this.com = com;
+	this.st = st;
+	this.trace = trace;
+}
+
+exports.fund=fund;
---
+++ fx/restserver/model/fund_dao.js
@@ -0,0 +1,21 @@
+﻿var Fund = require('./fund').fund, sqlite3 = require('sqlite3');
+
+function fund_dao(){
+	var db = new sqlite3.Database("C:/rou/db/abc.db");
+
+	this.list = function(id, params, callback){
+		var funds = [];
+		db.all('select code,name2,type,mgr,com,st,trace from fund_b order by code', 
+			function(err, rows, fields) {
+			if (err) throw err;
+		    for(var i=0; i<rows.length; i++){
+				var fund = new Fund(rows[i].code, rows[i].name2, rows[i].type, rows[i].mgr, rows[i].com, rows[i].st, rows[i].trace);
+				funds.push(fund);
+			}
+			callback(funds);
+			db.close();
+		});
+	};
+}
+
+exports.dao = fund_dao;
